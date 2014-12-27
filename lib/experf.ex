c8e0d86942a8fae6b527d9b894ff9869a6a31470
Bradley Smith <bradleydsmith@gmail.com>
2014-12-26 19:05:15 -0700

    add coordinator.  Start coordinator before spawning Experf.Http.

--- lib/experf.ex
+++ lib/experf.ex
@@ -3,7 +3,9 @@ defmodule Experf do
 
   def main(args) do
     options = parse_args(args)
+    coordinate_task = Task.async(Experf.Coordinator, :start, [options[:n]])
     do_requests(options[:n], options[:url])
+    Task.await(coordinate_task, :infinity)
   end
 
   defp parse_args(args) do
@@ -13,7 +15,7 @@ defmodule Experf do
 
   defp do_requests(n, url) do
     Enum.each(1..n, fn(i) ->
-      Experf.Http.request(i, url)
+      spawn Experf.Http, :request, [i, url]
     end)
   end
 end
